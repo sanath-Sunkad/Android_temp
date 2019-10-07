66ee5f1bf607b4cb68d2e2d792e847f68dd8db0e
sanath-Sunkad <sanathsunkad07@gmail.com>
2019-10-07 22:26:37 +0530

    original

--- Fragment/app/src/main/java/com/example/fragment/DetailActivity.java
+++ Fragment/app/src/main/java/com/example/fragment/DetailActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String DESC_ID="id";
@@ -12,9 +14,9 @@ public class DetailActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
-        DescriptionDetailFragment newDetailFraf=(DescriptionDetailFragment)getSupportFragmentManager().findFragmentById(R.id.detailfrag);
-        int descID=(int)getIntent().getExtras().get(DESC_ID);
-        newDetailFraf.setDetailDescID(descID);
+        DescriptionDetailFragment newDetailFrag = (DescriptionDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detailfrag);
+        int descID=(int) Objects.requireNonNull(getIntent().getExtras()).get(DESC_ID);
+        Objects.requireNonNull(newDetailFrag).setDetailDescID(descID);
 
     }
 }
